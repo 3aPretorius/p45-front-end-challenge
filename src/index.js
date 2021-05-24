ef4287526a7b9dc167fd07bf8273d5536a75298a
Andrea Pretorius <an3avj@gmail.com>
2021-05-24 15:57:39 +0200

    Basic skeleton layout of header and form

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import UserProfileForm from './components/user-profile-form/user-profile-form';
+import UserProfileForm from './components/user-profile-page/user-profile-page';
 
 document.addEventListener('DOMContentLoaded', function () {
   UserProfileForm.init();
